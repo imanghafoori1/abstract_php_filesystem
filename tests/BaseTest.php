1ad142a2cb0c24a19004e1ac0da879b8bd8a941c
Iman Ghafoori <imanghafoori1@gmail.com>
2022-01-29 23:44:44 +0330

    added tests for insertNewLine

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -14,12 +14,17 @@ class BaseTest extends TestCase
     public function removeLine()
     {
         FileSystem::fake();
-        FileManipulator::removeLine(__DIR__.'/stub/sample.stub', 7);
+        $result = FileSystem::removeLine(__DIR__.'/stub/sample.stub', 7);
 
-        $f1 = FileSystem::read_file(__DIR__.'/stub/sample.stub');
-        $f2 = FileSystem::read_file(__DIR__.'/stub/sample_removed_line.stub');
+        $f1 = FileSystem::file_get_content(__DIR__.'/stub/sample.stub', "\n");
+        $f2 = FileSystem::file_get_content(__DIR__.'/stub/sample_removed_line.stub', "\n");
 
         $this->assertEquals($f2, $f1);
+        $this->assertEquals($result, true);
+
+        FileSystem::fake();
+        $result = FileSystem::removeLine(__DIR__.'/stub/sample.stub', 700);
+        $this->assertEquals($result, false);
     }
 
     /**
@@ -28,10 +33,38 @@ class BaseTest extends TestCase
     public function removeLine2()
     {
         FileSystem::fake();
-        FileManipulator::removeLine(__DIR__.'/stub/sample_removed_line.stub', 3);
+        FileSystem::removeLine(__DIR__.'/stub/sample_removed_line.stub', 3);
 
-        $f1 = FileSystem::read_file(__DIR__.'/stub/sample_removed_line.stub');
-        $f2 = FileSystem::read_file(__DIR__.'/stub/sample_removed_line2.stub');
+        $f1 = FileSystem::file_get_content(__DIR__.'/stub/sample_removed_line.stub', "\n");
+        $f2 = FileSystem::file_get_content(__DIR__.'/stub/sample_removed_line2.stub', "\n");
+
+        $this->assertEquals($f2, $f1);
+    }
+
+    /**
+     * @test
+     */
+    public function insertLine()
+    {
+        FileSystem::fake();
+        FileSystem::insertNewLine(__DIR__.'/stub/sample.stub', 'Hello', 3);
+
+        $f1 = FileSystem::file_get_content(__DIR__.'/stub/sample.stub', "\n");
+        $f2 = FileSystem::file_get_content(__DIR__.'/stub/sample_inserted_hello.stub', "\n");
+
+        $this->assertEquals($f2, $f1);
+    }
+
+    /**
+     * @test
+     */
+    public function insertLine2()
+    {
+        FileSystem::fake();
+        FileSystem::insertNewLine(__DIR__.'/stub/sample.stub', 'Hello', 20);
+
+        $f1 = FileSystem::file_get_content(__DIR__.'/stub/sample.stub', "\n");
+        $f2 = FileSystem::file_get_content(__DIR__.'/stub/sample.stub', "\n");
 
         $this->assertEquals($f2, $f1);
     }
